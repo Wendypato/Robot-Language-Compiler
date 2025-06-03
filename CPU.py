e33bc4a9253c93efb583e92fa186e8ab3a28779f
JoseAlfredo0201 <A01645391@tec.mx>
2025-06-03 16:48:46 -0600

    Animation
    Added an animated penguin sprite to indicate the CPU movement

--- CPU.py
+++ CPU.py
@@ -11,9 +11,12 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 # Variables del Bot
-positionX = 0
-positionY = 0
+positionXF = 0
+positionYF = 0
+positionXA = 0
+positionYA = 0
 degrees = 0
+direccion = "DOWN"  # Direccion inicial del bot
 
 # Variables del archivo
 # Se abre el archivo y se lee su contenido
@@ -37,51 +40,91 @@ def dibujar_matrix():
                     pygame.draw.rect(window, BLACK, (j * 75, i * 75, 75, 75), 0)
 
 def check_position():
-    if positionX < 0 or positionX > 9 or positionY < 0 or positionY > 9:
-        print("El bot se ha salido de los límites del tablero.")
+    if positionXF < 0 or positionXF > 9 or positionYF < 0 or positionYF > 9:
+        print("El bot se ha salido de la matriz.")
         exit(1)  # Salir del programa si el bot se sale del tablero
         return 
 
+
 def bot_movement(num):
-    global positionY
-    global positionX
+    global positionYF
+    global positionXF
+    global positionYA
+    global positionXA
     global degrees
     if degrees == 0:  
-        positionY += num
+        positionYF += num
+        step = 0.25
     elif degrees == 90: 
-        positionX += num
+        positionXF += num
+        step = 0.25
     elif degrees == 180: 
-        positionY -= num
+        positionYF -= num
+        step = -0.25
     else: 
-        positionX -= num
+        positionXF -= num
+        step = -0.25
+    # Verificar si el movimiento es válido
+    check_position()
+    sprite_step = 1
+    # Animar el movimiento del bot
+    if direccion == "DOWN" or direccion == "UP":
+        for i in range(num*4):
+            round = i//4
+            positionYA += step
+            draw(i-4*round)
+    else:
+        for i in range(num*4):
+            round = i//4
+            positionXA += step
+            draw(i-4*round)
+    
+        
+                
 
 def rotate_bot(num):
     global degrees
+    global direccion
     degrees = degrees + num
     if degrees >= 360:
         degrees = degrees - 360
+    if degrees == 0:  
+        direccion = "DOWN"
+    elif degrees == 90: 
+        direccion = "RIGHT"
+    elif degrees == 180: 
+        direccion = "UP"
+    else: 
+        direccion = "LEFT"
 
 def  bot_logic():
     for i in range(len(instructions)):
         if instructions[i][0] == "MOV":
             bot_movement(int(instructions[i][1]))
             check_position()
-            draw()
+            draw(0)
         else:
             rotate_bot(int(instructions[i][1]))
+            draw(0)
     return 0
 
-def draw_bot():
-    pygame.draw.circle(window, (0, 0, 255), (positionX*75+37.5, positionY*75+37.5), 10)
+def draw_bot(num):
+    if num != 2:
+        CPU = pygame.image.load(f"sprites/penguin-gray[{direccion}][{num}].png")
+    else:
+        CPU = pygame.image.load(f"sprites/penguin-gray[{direccion}][{0}].png")
+    CPU = pygame.transform.scale(CPU, (75, 75))  # Escalar la imagen del bot
+    CPU.convert_alpha()  # Convertir la imagen para mejorar el rendimiento
+    window.blit(CPU, (positionXA * 75, positionYA * 75))
 
-def draw():
+def draw(num):
     dibujar_matrix()
-    draw_bot()
+    draw_bot(num)
     pygame.display.flip()  # Actualizar la pantalla
-    time.sleep(1)  # Esperar un poco para ver el cambio
+    time.sleep(.2)  # Esperar un poco para ver el cambio
 
 def main():
-    draw()
+    draw(0)
     bot_logic()
 
     while True:
